3e0319ed59fc23be959469e41ddf15aa922a2ba5
Styles <139181994+stylescode@users.noreply.github.com>
2023-08-28 21:46:30 -0700

    Add funcs to clear grid & create new grid

--- javascript.js
+++ javascript.js
@@ -1,17 +1,12 @@
-// 16x16 grid of square divs
-// 256 squares in total
-
-// maybe need 16 horizontal containers (rows)
-
 const container = document.querySelector('.container');
 
-function createGrid() {
-  for (let i = 1; i <= 16; i++) {
-    const row = document.createElement('div');
+function createGrid(amountSquares = 16) {
+  for (let i = 1; i <= amountSquares; i++) {
+    let row = document.createElement('div');
     container.appendChild(row);
     row.classList.add('row');
-    for (let i = 1; i <= 16; i++) {
-      const square = document.createElement('div');
+    for (let i = 1; i <= amountSquares; i++) {
+      let square = document.createElement('div');
       row.appendChild(square);
       square.classList.add('square');
     }
@@ -20,13 +15,32 @@ function createGrid() {
 
 createGrid();
 
-function fillSquare(e) {
+function fillSquare() {
   console.log(this);
   this.classList.add('hovered');
 }
 
-const squares = document.querySelectorAll('.square');
+let squares = document.querySelectorAll('.square');
 
 squares.forEach(square => square.addEventListener('mouseover', fillSquare));
 
+const editBtn = document.querySelector('.editButton');
+
+function clearGrid() {
+  container.innerHTML = '';
+}
+
+function editGrid() {
+  const userChoice = +prompt("How many squares would you like per side? (min: 2, max: 100)");
+  if (userChoice < 2 || userChoice > 100) {
+    alert('Input Cancelled');
+  } else {
+    clearGrid();
+    createGrid(userChoice);
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', fillSquare));
+  };
+}
+
+editBtn.addEventListener('click', editGrid);
 
